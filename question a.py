a5512a9b38ff2f950b8272149817dd51bcbb6b22
Justin Irungu <justin.irungu@strathmore.edu>
2025-05-16 13:32:57 +0300

    Add files via upload

---
+++ question a.py
@@ -0,0 +1,12 @@
+
+days = int(input("Enter the number of days: "))
+
+
+seconds = days * 24 * 60 * 60
+
+
+print(f"There are {seconds} seconds in {days} days.")
+
+
+
+
---
+++ question b.py
@@ -0,0 +1,4 @@
+Radius = float(input("Enter the radius of the sphere: "))
+volume = (Radius**3)*(4/3)*3.14
+
+print(f"The volume of the sphere is {volume} cubic units.")
