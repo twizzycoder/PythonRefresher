abf69a9f11adaa74ea8bded494794fcf2d68f5da
Justin Irungu <justin.irungu@strathmore.edu>
2025-05-17 23:21:36 +0300

    Add files via upload

---
+++ questtion c.py
@@ -0,0 +1,14 @@
+def calculate_area (length):
+    return length * length
+
+def calculate_perimeter(length):
+    return 2 * length+length
+    
+length = float(input("Enter the length of the square: "))
+
+area = calculate_area(length)
+perimeter = calculate_perimeter(length)
+
+print("The area of the square is = ", area)
+print("The perimeter of the square is = ", perimeter)
+   
